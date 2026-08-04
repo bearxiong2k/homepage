94938b76e217a49293ab666c4e5b2e4ca66f519e
Glados <d202281013@hust.edu.cn>
2026-08-05 01:03:23 +0800

    transparent navigator button

--- public/marginalia/app-version.js
+++ public/marginalia/app-version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = '20260805-003842';
-export const APP_VERSION_LABEL = 'Version 2026-08-05 00:38:42 CST';
-export const APP_VERSION_SHORT = '2026-08-05 00:38 CST';
+export const APP_VERSION = '20260805-005307';
+export const APP_VERSION_LABEL = 'Version 2026-08-05 00:53:07 CST';
+export const APP_VERSION_SHORT = '2026-08-05 00:53 CST';
